3f083ec74ec57022e9ecadf96078ee07a607827d
hanyu.wang <wanghanyu@mathmagical.com>
2025-11-26 17:17:00 +0800

    birefnet-portrait custom path

--- image_utils/rem_bg_local.py
+++ image_utils/rem_bg_local.py
@@ -1,10 +1,20 @@
-from rembg import remove
+import rembg
 
-input_path = 'input.png'
-output_path = 'output.png'
+#
+# input_path = 'input/cup.jpg'
+# output_path = 'output/cup_rembg.jpg'
+#
+# with open(input_path, 'rb') as i:
+#     with open(output_path, 'wb') as o:
+#         input = i.read()
+#         output = remove(input, force_return_bytes=True)
+#         o.write(output)
 
-with open(input_path, 'rb') as i:
-    with open(output_path, 'wb') as o:
-        input = i.read()
-        output = remove(input, force_return_bytes=True)
-        o.write(output)
+
+input_path = 'input/cup.jpg'
+output_path = 'output/cup_rembg.jpg'
+input = open(input_path, "rb").read()
+session = rembg.new_session("birefnet-portrait",**{"model_path": "/Users/rogerswang/.u2net/birefnet-portrait.onnx"})
+output = rembg.remove(input, session=session)
+with open(output_path, "wb") as o:
+    o.write(output)
