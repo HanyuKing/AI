a4c077ae3497d4368327a44dbea232c4b9b1d0d9
hanyu.wang <wanghanyu@mathmagical.com>
2025-12-01 10:54:27 +0800

    port:8000

--- server/app.py
+++ server/app.py
@@ -63,4 +63,4 @@ async def tool_base64(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("server.app:app", host="0.0.0.0", port=80, reload=True)
+    uvicorn.run("server.app:app", host="0.0.0.0", port=8000, reload=True)
