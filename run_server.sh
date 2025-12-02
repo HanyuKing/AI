c9c18096cf9b82fc0e099f5d64dbb5b8f6763191
hanyu.wang <wanghanyu@mathmagical.com>
2025-12-02 17:12:30 +0800

    exclude pkgs

--- run_server.sh
+++ run_server.sh
@@ -3,5 +3,5 @@ cd "$(dirname "$0")"
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 echo "Starting MediaToolbox Server..."
 echo "Visit http://localhost:8000 to use the tools."
-uvicorn server.app:app --host 0.0.0.0 --port 8000 --reload --reload-exclude "*/ENTER/*"
+uvicorn server.app:app --host 0.0.0.0 --port 8000 --reload --reload-exclude "**/ENTER/**"
 
