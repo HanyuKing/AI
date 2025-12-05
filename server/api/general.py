cee18ae3a3bd1bb2bf537720beb0b2d122c65593
hanyu.wang <wanghanyu@mathmagical.com>
2025-12-05 17:45:02 +0800

    front

--- server/api/general.py
+++ server/api/general.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Response
 from server.schemas.feedback import FeedbackRequest
 from server.services.feedback_service import feedback_service
 
@@ -15,4 +15,57 @@ async def submit_feedback(request: FeedbackRequest):
         return {"status": "success", "id": feedback_id}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+@router.get("/sitemap.xml", include_in_schema=False)
+async def sitemap():
+    """
+    Generate sitemap.xml for SEO
+    """
+    base_url = "https://mediatoolbox.com"
+    urls = [
+        "/",
+        "/tools/id-photo",
+        "/tools/pdf-compress",
+        "/tools/image-convert",
+        "/tools/json",
+        "/tools/base64",
+        "/tools/uuid",
+        "/tools/timestamp",
+        "/tools/qrcode",
+        "/tools/image-edit",
+        "/tools/base-convert",
+        "/tools/url-encoder",
+        "/tools/password",
+        "/tools/diff",
+        "/tools/svg-converter",
+        "/feedback"
+    ]
+    
+    xml_content = '<?xml version="1.0" encoding="UTF-8"?>\n'
+    xml_content += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
+    
+    for url in urls:
+        xml_content += '  <url>\n'
+        xml_content += f'    <loc>{base_url}{url}</loc>\n'
+        xml_content += '    <changefreq>weekly</changefreq>\n'
+        xml_content += '    <priority>0.8</priority>\n'
+        xml_content += '  </url>\n'
+        
+    xml_content += '</urlset>'
+    
+    return Response(content=xml_content, media_type="application/xml")
+
+@router.get("/robots.txt", include_in_schema=False)
+async def robots():
+    """
+    Generate robots.txt for SEO
+    """
+    content = """User-agent: *
+Allow: /
+Disallow: /api/
+Disallow: /static/private/
+
+Sitemap: https://mediatoolbox.com/sitemap.xml
+"""
+    return Response(content=content, media_type="text/plain")
 
